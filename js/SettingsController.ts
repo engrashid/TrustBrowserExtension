6995088104296f17e32c12217a96d811b8851125
engrashid <rashid.rob@hotmail.com>
2018-09-18 21:10:02 +0600

    migrates settingscontroller to ts

--- js/SettingsController.ts
+++ js/SettingsController.ts
@@ -1,59 +1,4 @@
-//  declare var tce: any;
-//  var SettingsController = function() {
-//     function SettingsController()
-//     {
-//         var self = this;
-//         this.createSettings = function() {
-//             var settings = {
-//                 "password": '',
-//                 "seed": '',
-//                 "rememberme": true,
-//                 "infoserver": "https://trust.dance",
-//                 // "buildserver": "https://trust.dance:12701",
-//                 // "graphserver": "https://trust.dance:12702",
-//                 'trustrender': 'icon',
-//                 "resultrender": 'warning'
-//                 //"keypair": null
-//             }
-//             return settings;
-//         }
-    
-//         this.saveSettings = function(settings) {
-//             if (settings.rememberme) {
-//                 settings.keyPair = undefined;
-//                 chrome.storage.local.set({ usersettings: settings }, function () {
-//                     this.buildKey(settings);
-//                     console.log('Settings saved');
-
-//                 });
-//             }
-//         }
-    
-//         this.loadSettings = function (cb) {
-//             chrome.storage.local.get('usersettings', function (result) {
-//                 var settings = (result.usersettings) ? result.usersettings : self.createSettings();
-//                 self.buildKey(settings);
-//                 cb(settings);
-//             });
-//         }
-    
-//         this.buildKey = function (settings) {
-//             var keystring = settings.password + settings.seed;
-//             var hash = tce.bitcoin.crypto.hash256(keystring);
-//             var d = tce.BigInteger.fromBuffer(hash)
-            
-//             settings.keyPair = new tce.bitcoin.ECPair(d)
-//             settings.address = settings.keyPair.getAddress();
-//             return settings.keyPair;
-//         }
-//     }
-
-//     return SettingsController;
-// }
-// export = SettingsController;
- //module.exports = { SettingsController}
  declare var tce: any;
-
  class SettingsController {
 
     constructor(){
@@ -62,7 +7,7 @@
        // const self = this;
     }
     createSettings = (): any => {
-        var settings = {
+        const settings = {
             "password": '',
             "seed": '',
             "rememberme": true,
@@ -90,16 +35,16 @@
     loadSettings = (cb): any => {
         console.log('ts settings working')
         chrome.storage.local.get('usersettings', (result) => {
-            var settings = (result.usersettings) ? result.usersettings : this.createSettings;
+            let settings = (result.usersettings) ? result.usersettings : this.createSettings;
             this.buildKey(settings);
             cb(settings);
         });
     }
 
    public buildKey = (settings): any => {
-        var keystring = settings.password + settings.seed;
-        var hash = tce.bitcoin.crypto.hash256(keystring);
-        var d = tce.BigInteger.fromBuffer(hash)
+        let keystring = settings.password + settings.seed;
+        let hash = tce.bitcoin.crypto.hash256(keystring);
+        let d = tce.BigInteger.fromBuffer(hash)
         
         settings.keyPair = new tce.bitcoin.ECPair(d)
         settings.address = settings.keyPair.getAddress();
