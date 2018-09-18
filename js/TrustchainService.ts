7bfa1653c86c44e5e757eebd4ab0d5a01356ba44
engrashid <rashid.rob@hotmail.com>
2018-09-19 01:05:03 +0600

    migrates trust chain to ts

---
+++ js/TrustchainService.ts
@@ -0,0 +1,149 @@
+///<reference path="../typings/globals/jquery/index.d.ts" />
+
+class TrustchainService  {
+    settings: any;
+    constructor(settings) {
+        this.settings = settings;
+    } 
+
+    Query (targets) {
+        let query = this.BuildQuery(targets, null);
+        if(query == null) {
+            let deferred = $.Deferred();
+            deferred.resolve(null);
+            return deferred;
+        }
+            
+        return this.PostData('/api/graph/Query', JSON.stringify(query));
+    }
+
+    BuildQuery (targets, scope) {
+        let subjects = [];
+        for (let key in targets) {
+            if (!targets.hasOwnProperty(key))
+                continue;
+                // do stuff
+            let target = targets[key];
+            let subject = { address: target.address };
+            subjects.push(subject);
+            if(target.owner && target.owner.address) {
+                subject = { address: target.owner.address };
+                subjects.push(subject);
+            }
+        }
+
+        if(subjects.length == 0)
+            return null;
+    
+        if(typeof scope === 'string')
+            scope = { value : scope };
+
+        let obj = {
+            "issuers": this.settings.address,
+            "subjects": subjects,
+    
+            // Scope is used to filter on trust resolvement. It can be any text
+            "scope": (scope) ? scope : undefined, // The scope could also be specefic to country, area, products, articles or social medias etc.
+    
+            // Claim made about the subject. The format is specified by the version property in the header section.
+            "types": [
+                "binary.trust.dtp1",
+                "alias.identity.dtp1"
+              ],
+            "level": 0, // Use default level search (0)
+            //"flags": "LeafsOnly"
+        }
+        return obj;
+    }
+
+    GetTrustById (id) {
+        let url ='/api/trust/get/'+id; // id = encoded byte array
+    
+        return this.GetData(url);
+    }
+
+    GetSimilarTrust (trust) {
+        let url ='/api/trust/get/?issuer='+trust.issuer.address+'&subject='+trust.subject.address+'&type='+encodeURIComponent(trust.type)+'&scopevalue='+encodeURIComponent((trust.scope) ? trust.scope.value : "");
+    
+        return this.GetData(url);
+    }
+
+
+    GetTrustTemplate (subject, alias) {
+        let url ='/api/trust/build?issuer='+settings.address+'&subject='+subject+'&alias='+alias;
+    
+        return this.GetData(url);
+    }
+
+    // PostTrustTemplate = function(trust) {
+    //     return this.PostData('/api/trust/build', JSON.stringify(trust));
+    // }
+
+    PostTrustTemplate (trustPackage) {
+        return this.PostData('/api/package/build', JSON.stringify(trustPackage));
+    }
+
+    PostTrust (trust) {
+        return this.PostData('/api/trust/add', JSON.stringify(trust));
+    }
+    
+    GetData (query) {
+        let deferred = $.Deferred();
+        //let self = this;
+        let url = this.settings.infoserver + query;
+
+        $.ajax({
+            type: "GET",
+            url: url,
+            contentType: 'application/json; charset=utf-8',
+        }).done(function (msg, textStatus, jqXHR) {
+           let resolve = msg;
+            deferred.resolve(resolve);
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            this.TrustServerErrorAlert(jqXHR, textStatus, errorThrown, this.settings.infoserver);
+            deferred.fail();
+        });
+
+        return deferred.promise();
+    }
+
+
+    PostData = (query, data) => {
+        var deferred = $.Deferred();
+        //var self = this;
+        let url = this.settings['infoserver'] + query;
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: data,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json'
+        }).done(function (msg, textStatus, jqXHR) {
+            let resolve = msg;
+            deferred.resolve(resolve);
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            this.TrustServerErrorAlert(jqXHR, textStatus, errorThrown, this.settings.infoserver);
+            deferred.fail();
+        });
+
+        return deferred.promise();
+    }
+
+    TrustServerErrorAlert (jqXHR, textStatus, errorThrown, server) {
+        if (jqXHR.status == 404 || errorThrown == 'Not Found') {
+            var msg = 'Error 404: Server ' + server + ' was not found.';
+            //alert('Error 404: Server ' + server + ' was not found.');
+            console.log(msg);
+        }
+        else {
+            var msg = textStatus + " : " + errorThrown;
+            if (jqXHR.responseJSON && jqXHR.responseJSON.ExceptionMessage)
+                msg = jqXHR.responseJSON.ExceptionMessage;
+    
+            alert(msg);
+        }
+    }
+
+}
+export = TrustchainService
